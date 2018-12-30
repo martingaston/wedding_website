100f9979bcd03aa3f1854d535e7f696a672389a4
martingaston <squidman@gmail.com>
2018-12-30 15:46:16 +0000

    add code for netlify robots to play nice with gatsby

--- src/pages/rsvp.js
+++ src/pages/rsvp.js
@@ -13,8 +13,8 @@ export default class Rsvp extends React.Component {
   }
 
   handleGuest(e) {
-    this.setState((state, props) => { 
-        return { toggle: !state.toggle }
+    this.setState((state, props) => {
+      return { toggle: !state.toggle };
     });
   }
 
@@ -33,40 +33,52 @@ export default class Rsvp extends React.Component {
         <p>
           We'll also pop your e-mail into a mailing list so we can easily
           contact everyone with updates and gossip, but if you'd prefer not to
-          be contacted this way then that's also fine - just let us know and we'll take you off.
+          be contacted this way then that's also fine - just let us know and
+          we'll take you off.
         </p>
         <p>
           If you could also let us know your choices by <b>April 15</b> that
-          will give us enough time to both sort everything out and get very excited about seeing you.
+          will give us enough time to both sort everything out and get very
+          excited about seeing you.
         </p>
-        <form name="rsvp" id="rsvp" method="POST" action="/rsvp-success" data-netlify="true" netlify-honeypot="bot-field">
+        <form
+          name="rsvp"
+          id="rsvp"
+          method="POST"
+          action="/rsvp-success"
+          data-netlify="true"
+          netlify-honeypot="bot-field"
+        >
+          <input type="hidden" name="form-name" value="rsvp" />
           <FormData id="first" pronoun="you" adjective="your" />
           <div>
-              Are you also responding for another guest?
-              <ul>
-                <li>
-                  <input
-                    type="radio"
-                    id="guest_no"
-                    name="guest"
-                    onChange={this.handleGuest}
-                    checked={!this.state.toggle}
-                  />
-                  <label for="guest_no">No, just me</label>
-                </li>
-                <li>
-                  <input
-                    type="radio"
-                    id="guest_yes"
-                    name="guest"
-                    onChange={this.handleGuest}
-                    checked={this.state.toggle}
-                  />
-                  <label for="guest_yes">Yes, they're very lazy</label>
-                </li>
-              </ul>
+            Are you also responding for another guest?
+            <ul>
+              <li>
+                <input
+                  type="radio"
+                  id="guest_no"
+                  name="guest"
+                  onChange={this.handleGuest}
+                  checked={!this.state.toggle}
+                />
+                <label for="guest_no">No, just me</label>
+              </li>
+              <li>
+                <input
+                  type="radio"
+                  id="guest_yes"
+                  name="guest"
+                  onChange={this.handleGuest}
+                  checked={this.state.toggle}
+                />
+                <label for="guest_yes">Yes, they're very lazy</label>
+              </li>
+            </ul>
           </div>
-          {this.state.toggle && <FormData id="second" pronoun="they" adjective="their" />}
+          {this.state.toggle && (
+            <FormData id="second" pronoun="they" adjective="their" />
+          )}
           <div>
             <label for="more">
               Is there anything else you'd like to let us know?
